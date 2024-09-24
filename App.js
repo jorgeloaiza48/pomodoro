63b456e2d6319ed53b3354796e59248f751c554c
jorgeloaiza48 <jelm48@misena.edu.co>
2024-09-24 10:05:08 -0500

    mejorando

--- App.js
+++ App.js
@@ -5,17 +5,19 @@ import Header from './src/components/Header';
 
 const colors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"]
 
+
 export default function App() {
   const [isWorking, setIsworking] = useState(false)
   const [time, seTime] = useState(25 * 60)
   const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK")
 
   return (
-    <View style={styles.container}>      
+    <View style={[styles.container,{backgroundColor:colors[currentTime]}]}>      
       <Text style={styles.text}>Pomodoro</Text>
+      <Text>{time}</Text>
       <StatusBar style="light" />
       {/* StatusBar es la barra que muestra la hora, batería, señal, etc */}
-      <Header currentTime={currentTime} setCurrentTime={setCurrentTime} time={time} />
+      <Header currentTime={currentTime} setCurrentTime={setCurrentTime} setTime={seTime} />
     </View>
   );
 }
@@ -23,10 +25,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 40
+    paddingTop: 40,
+    backgroundColor:"#F7DC6F"
   },
   text: {
     fontSize: 32,
-    fontWeight: "bold"
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingBottom:20
   }
 });
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,22 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native"
 const options = ["Pomodoro", "Short Break", "Long Break"]
 
 
-export default function Header({currentTime, setCurrentTime, time}) {
+export default function Header({currentTime, setCurrentTime, setTime}) {
+
+    // Esta función analiza en qué botón hicieron click. Si fue en el primero entonces index vale cero y le asigna a newTime el valor de 25. Si fue en el segundo entonces index vale 1 y asigna a newTime el valor de 5. Sino asigna el valor de 15.
+    //Luego setea la variable currentTime al mismo valor de index.
+    //Luego setea la variable time al valor de newTime * 60
+    function handlePress(index){
+        const newTime = index === 0 ? 25:index === 1 ? 5: 15;
+        setCurrentTime(index);
+        setTime(newTime * 60)
+    }
     return (
         <View style={styles.view}>
             {options.map((item, index) => (
-                <TouchableOpacity key={index} style={styles.itemStyle}>
-                    <Text>{item}</Text>
+                // currentTime != index && {borderColor:"transparent" -->> establece border transparente a los otros dos botones en los que no hicieron click.
+                <TouchableOpacity key={index} style={[styles.itemStyle,currentTime != index && {borderColor:"transparent"}]} onPress={()=>handlePress(index)}> 
+                    <Text style={styles.text}>{item}</Text>                    
                 </TouchableOpacity>
             ))}
         </View>
@@ -22,7 +32,14 @@ const styles = StyleSheet.create({
     },
     itemStyle: {        
         width:"33%",
-        borderWidth:3,
+        borderWidth:2,
         padding:5,
+        borderRadius:10,
+        borderColor:"white",
+        marginVertical:20
+    },
+    text:{
+        textAlign:"center",
+        fontWeight:"bold"
     }
 })
